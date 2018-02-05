72793c2ffc7e8de41ab54cdbb6a1eb9aca70bcc9
benderham <ben@benderham.com>
2018-02-05 09:00:38 +0800

    add video component skeleton

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-
 import Button from './Button';
 
 storiesOf('Buttons', module)
